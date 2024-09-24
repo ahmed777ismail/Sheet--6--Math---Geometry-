733f92bf56c19db0199f6cc6c8dfd6cb95533f75
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-09-24 06:49:41 +0300

    Add files via upload
    Factorial Digits

---
+++ Factorial Digits/Factorial Digits/Factorial Digits.cpp
@@ -0,0 +1,11 @@
+//#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main() {
+	double num, result;
+	cin >> num;
+	for (int i = 2; i <= num; i++) {
+		result += log10(i);
+	}
+	cout << "Number of digits of " << num << "! is " << (int)result + 1 << endl;
+}
